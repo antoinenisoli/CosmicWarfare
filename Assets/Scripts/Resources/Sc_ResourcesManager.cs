8fa0084e9074411d700daa58e701de9c51f0aed7
antoinenisoli <nisoliantoine@outlook.com>
2020-12-03 09:42:41 +0100

    add assets

--- Assets/Scripts/Resources/Sc_ResourcesManager.cs
+++ Assets/Scripts/Resources/Sc_ResourcesManager.cs
@@ -1,18 +1,61 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum ResourceType
+{
+    Wood,
+    Oil,
+}
 
 public class Sc_ResourcesManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Serializable]
+    class Resource
     {
-        
+        public string name;
+        public Text displayResource;
+        [SerializeField] int currentAmount;
+        [SerializeField] int maxAmount;
+        public int CurrentAmount
+        {
+            get => currentAmount;
+
+            set
+            {
+                if (value > maxAmount)
+                    value = maxAmount;
+
+                if (value < 0)
+                    value = 0;
+
+                currentAmount = value;
+            }
+        }
+
+        public int MaxAmount { get => maxAmount; set => maxAmount = value; }
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] Resource[] resources;
+
+    private void Start()
     {
-        
+        ActualizeText();
+    }
+
+    public void Cost(ResourceType res, int amount)
+    {
+        resources[(int)res].CurrentAmount -= amount;
+        ActualizeText();
+    }
+
+    public void ActualizeText()
+    {
+        for (int i = 0; i < resources.Length; i++)
+        {
+            resources[i].displayResource.text = resources[i].name + " : " + resources[i].CurrentAmount;
+        }
     }
 }
